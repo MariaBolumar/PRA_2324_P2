314019315d0d18a90f6fb0fb9e8a3f9cb96535e5
Pepita <Pepita@palotes.com>
2024-12-03 13:04:26 +0100

    Ahora ya van tanto la función BusquedaBinaria como QuickSort, falta que no sean int sino T

--- test_DyV.cpp
+++ test_DyV.cpp
@@ -2,29 +2,31 @@
 
 
 int main() {
-    	std::vector<int> V(6);
+    	std::vector<int> V(5);
     	int X, resultado;
 
 	std::cout << "Introduce el vector: ";
-    	for(int i = 0; i< 6; i++){
+    	for(int i = 0; i< 5; i++){
 		std::cin>> V[i];
 	}
 	std::cout << "\nIntroduce el valor a buscar: ";
 	std::cin>>X;
-
-	resultado = BusquedaBinaria(X, V, 0, V.size() - 1);
 	
-	if (resultado != -1) {
-        	std::cout << "El valor " << X << " se encuentra en la posición " << resultado << std::endl;
-    	} else {
-        	std::cout << "El valor " << X << " no se encuentra en el array" << std::endl;
-	}
 	QuickSort(V, 0, V.size() - 1);
     	std::cout << "Vector ordenado: ";
     	for (int i = 0; i < V.size(); ++i) {
         	std::cout << V[i] << " ";
     	}
-    	std::cout << std::endl;
+	std::cout << "\n";
+
+	resultado = BusquedaBinaria(X, V, 0, V.size() - 1);
+	
+	if (resultado != -1) {
+        	std::cout << "El valor " << X << " se encuentra en la posición " << resultado+1 << std::endl;
+    	} else {
+        	std::cout << "El valor " << X << " no se encuentra en el array" << std::endl;
+	}
+
 
 	return 0;
 }
